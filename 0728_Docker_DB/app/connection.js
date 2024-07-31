067f4c818a460adeabaec9bf484f29828c9034e2
ShimHyesu <hesushim@gmail.com>
2024-07-31 20:26:01 +0900

    Docker - MariaDB 연결

--- 0728_Docker_DB/app/connection.js
+++ 0728_Docker_DB/app/connection.js
@@ -1,25 +1,28 @@
 //
-// MySQL 데이터베이스와의 연결을 설정하는 파일
+// 데이터베이스와의 연결을 설정하는 파일
 //
 
-const mysql = require('mysql2');
+const mariadb = require('mariadb');
 require('dotenv').config({path: '../.env'});
 
-const dbUser = process.env.MYSQL_USER
-const dbPassword = process.env.MYSQL_PASSWORD
-const dbName = process.env.MYSQL_DATABASE
+const db = process.env.DB_DATABASE
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
 
-// MySQL 연결 풀을 생성
+// 연결 풀을 생성
 // 여러 개의 연결을 미리 열어 두고 요청이 있을 때마다 재사용
-const pool = mysql.createPool({ 
-    host: 'db', // MySQL 호스트명 (Docker Compose 설정에서 'db' 서비스)
+const pool = mariadb.createPool({ 
+    host: 'db', // Docker Compose 설정에서 'db' 서비스
     user: dbUser,
     password: dbPassword,
-    database: dbName,
-    waitForConnections: true, // 연결이 가득 차면 대기할지 여부
+    database: db,
     connectionLimit: 10, // 최대 연결 수
-    queueLimit: 0 // 대기열의 최대 크기 (0은 무제한)
 })
 
+async function fetchConn(){
+    let conn = await pool.getConnection();
+    return conn;
+}
 
-module.exports = pool.promise(); // Promise 기반의 MySQL 연결 풀을 반환
+
+module.exports = fetchConn;
--- 0728_Docker_DB/app/userRouter.js
+++ 0728_Docker_DB/app/userRouter.js
@@ -1,10 +1,10 @@
 //
 // Express.js 라우터 모듈
-// MySQL 데이터베이스와 상호작용하여 사용자 데이터를 조회하는 기능을 제공
+// 데이터베이스와 상호작용하여 사용자 데이터를 조회하는 기능을 제공
 //
 
 const express = require('express');
-const pool = require('./connection');
+const fetchConn = require('./connection');
 
 const userRouter = express.Router();
 
@@ -13,12 +13,17 @@ userRouter.get('/', (req, res) => {
 })
 
 userRouter.get('/getUsers', async (req, res) => {
+    let conn;
     try {
-        const [results] = await pool.query('SELECT * FROM users');
+        conn = await fetchConn();
+
+        const [results] = await conn.query('SELECT * FROM users');
         res.json(results);
     } catch (err) {
         console.error('Error fetching users:', err);
         res.status(500).send('Error fetching users');
+    }finally {
+        if(conn) conn.end();
     }
 });
 
