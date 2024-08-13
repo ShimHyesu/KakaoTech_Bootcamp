328937947e49ee49736d908fde0cab6d0ae88d57
ShimHyesu <hesushim@gmail.com>
2024-08-14 00:30:25 +0900

    ELK: 로컬

---
+++ Bunch/dev/main.tf
@@ -0,0 +1,203 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "192.166.0.0/16"
+
+  tags = {
+    Name = "bunch_dev_vpc"
+  }
+}
+
+
+
+# Subnet
+resource "aws_subnet" "subnet_2a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.166.1.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "bunch_dev_subnet_2a_public"
+  }
+}
+
+
+
+# Public Route
+resource "aws_internet_gateway" "igw_public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "bunch_dev_igw_public"
+  }
+}
+
+resource "aws_route_table" "rtb_public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_public.id
+  }
+
+  tags = {
+    Name = "bunch_dev_rtb_public"
+  }
+}
+
+resource "aws_route_table_association" "rtb_association_public" {
+  subnet_id      = aws_subnet.subnet_2a.id
+  route_table_id = aws_route_table.rtb_public.id
+}
+
+
+
+# Security Group 
+# TODO: 나중에 따로 만들기
+resource "aws_security_group" "sg" {
+  name        = "bunch_dev_sg"
+  description = "Security group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { # ping - 모든 ICMP, 내 VPC
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["192.166.0.0/16"] 
+  }
+
+  ingress { # kafka
+    from_port = 19092
+    to_port = 19092
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { # kafka-ui
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_elk" {
+  name        = "bunch_dev_sg_elk"
+  description = "Security group for ELK" 
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { # ping - 모든 ICMP, 내 VPC
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["192.166.0.0/16"] 
+  }
+
+  ingress { # elasticsearch
+    from_port = 9200
+    to_port = 9200
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { # logstash
+    from_port = 5044
+    to_port = 5044
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { # kibana
+    from_port = 5601
+    to_port = 5601
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# EC2 Instance
+resource "aws_instance" "server_instance" {
+  count         = 1
+  ami           = "ami-045f2d6eeb07ce8c0" # amazon linux
+  instance_type = "t2.micro"
+  key_name      = "bunch-key"
+  subnet_id     = aws_subnet.subnet_2a.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "bunch_dev_server_instance"
+  }
+}
+
+resource "aws_instance" "mariadb_instance" {
+  ami           = "ami-045f2d6eeb07ce8c0" # amazon linux
+  instance_type = "t3.medium"
+  key_name      = "bunch-key"
+  subnet_id     = aws_subnet.subnet_2a.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "bunch_dev_mariadb_instance"
+  }
+}
+
+resource "aws_instance" "kafka_instance" {
+  ami           = "ami-045f2d6eeb07ce8c0" # amazon linux
+  instance_type = "t3.large"
+  key_name      = "bunch-key"
+  subnet_id     = aws_subnet.subnet_2a.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "bunch_dev_kafka_instance"
+  }
+}
+
+resource "aws_instance" "elk_instance" {
+  ami           = "ami-045f2d6eeb07ce8c0" # amazon linux
+  instance_type = "m5.xlarge"
+  key_name      = "bunch-key"
+  subnet_id     = aws_subnet.subnet_2a.id
+  vpc_security_group_ids = [aws_security_group.sg_elk.id]
+  
+  root_block_device {
+    volume_size = 20  # 루트 볼륨 크기를 20GB로 설정
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "bunch_dev_elk_instance"
+  }
+}
